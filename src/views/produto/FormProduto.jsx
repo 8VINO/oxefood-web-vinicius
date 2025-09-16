fbad3305c0f663609af9a5c121493a3ea63b136f
Aluno <aluno@ifpe.com>
2025-09-16 16:56:13 -0300

    feat: add views folder

---
+++ src/views/produto/FormProduto.jsx
@@ -0,0 +1,14 @@
+import InputMask from 'comigo-tech-react-input-mask';
+import React from "react";
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+
+export default function FormProduto(){
+    return(
+        
+        <div style={{marginTop:'3%'}}>
+            <Container textAlign='justified'>
+                <h2><span>Hello</span></h2>
+            </Container>
+        </div>
+    )
+}
